6352e1c1e63d4b041aea9e60e97de91f2eb0d953
jclarytx3 <54626757+jclarytx3@users.noreply.github.com>
2019-11-23 08:21:11 -0600

    Initial Semester Project

---
+++ js/welcome.js
@@ -0,0 +1,6 @@
+function trigger() {
+    document.getElementById("welcome").addEventListener("mouseover", popup);
+    function popup() {
+        alert("Welcome to Jim Clary's Webpage!!!");
+    }
+}
